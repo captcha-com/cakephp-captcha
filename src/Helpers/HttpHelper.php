829ed3d4ccd09c3d3ed00feb885c7308770e5e00
BotDetect Captcha <botdetect.support@captcha.com>
2015-10-30 23:26:37 +0700

    added HttpHelper class

---
+++ src/Helpers/HttpHelper.php
@@ -0,0 +1,33 @@
+<?php namespace CakeCaptcha\Helpers;
+
+final class HttpHelper {
+
+    /**
+     * Disable instance creation.
+     */
+    private function __construct() {}
+    
+    /**
+     * @return void
+     */
+    public static function AllowCache() {
+        header('Cache-Control: public');
+        header_remove('Expires');
+        header_remove('Pragma');
+    }
+
+    /**
+     * Throw a bad request.
+     *
+     * @param string  $p_Message
+     * @return void
+     */
+    public static function BadRequest($p_Message) {
+        while (ob_get_contents()) { ob_end_clean(); }
+        header('HTTP/1.1 400 Bad Request');
+        header('Content-Type: text/plain');
+        echo $p_Message;
+        exit;
+    }
+
+}
