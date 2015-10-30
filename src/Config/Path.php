34fdf6e3784e1064eec83a8bc7162030d3b745ce
BotDetect Captcha <botdetect.support@captcha.com>
2015-10-30 18:39:49 +0700

    added Path class which defines some paths related to Library and CakeCaptcha package

---
+++ src/Config/Path.php
@@ -0,0 +1,46 @@
+<?php namespace CakeCaptcha\Config;
+
+final class Path {
+  
+    /**
+     * Disable instance creation.
+     */
+    private function __construct() {}
+    
+    /**
+     * Physical path of the captcha-com/captcha package.
+     *
+     * @return string
+     */
+    public static function GetLibPackageDirPath() {
+        return __DIR__ . '/../../../captcha/';
+    }
+    
+    /**
+     * Physical path of public derectory which is located inside the captcha-com/captcha package.
+     *
+     * @return string
+     */
+    public static function GetPublicDirPathInLibrary() {
+        return self::GetLibPackageDirPath() . 'lib/botdetect/public/';
+    }
+    
+    /**
+     * Physical path of botdetect.php file which is located inside the captcha-com/captcha package.
+     *
+     * @return string
+     */
+    public static function GetBotDetectFilePathInLibrary() {
+        return self::GetLibPackageDirPath() . 'lib/botdetect.php';
+    }
+    
+    /**
+     * Physical path of captcha config defaults file.
+     *
+     * @return string
+     */
+    public static function GetCaptchaConfigDefaultsFilePath() {
+        return __DIR__ . '/CaptchaConfigDefaults.php';
+    }
+  
+}
