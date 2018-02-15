2c8d09c4239d5f08f407e4d2faaf35f89dcaefc9
botdetect.support <botdetect.support@captcha.com>
2018-02-15 14:43:52 +0700

    update code to compatible with new version of bd php

--- src/Controller/Component/CaptchaComponent.php
+++ src/Controller/Component/CaptchaComponent.php
@@ -56,6 +56,12 @@ class CaptchaComponent extends Component
 
         $captchaId = $params['captchaconfig'];
 
+        $captchaInstanceId = null;
+        if(isset($params['captchainstanceid'])) {
+            $captchaInstanceId = $params['captchainstanceid'];
+
+        }
+
         // get captcha config
         $config = UserCaptchaConfiguration::get($captchaId);
 
@@ -71,7 +77,7 @@ class CaptchaComponent extends Component
         UserCaptchaConfiguration::save($config);
         
         // init botdetect captcha instance
-        $this->initCaptcha($config);
+        $this->initCaptcha($config, $captchaInstanceId);
     }
 
     /**
@@ -80,11 +86,11 @@ class CaptchaComponent extends Component
      * @param  array  $config
      * @return void
      */
-    public function initCaptcha(array $config)
+    public function initCaptcha(array $config, $captchaInstanceId = null)
     {
         // set captchaId and create an instance of Captcha
         $captchaId = (array_key_exists('CaptchaId', $config)) ? $config['CaptchaId'] : 'defaultCaptchaId';
-        $this->captcha = new \Captcha($captchaId);
+        $this->captcha = new \Captcha($captchaId, $captchaInstanceId);
 
         // set user's input id
         if (array_key_exists('UserInputID', $config)) {
@@ -111,6 +117,17 @@ class CaptchaComponent extends Component
         if (method_exists($this->captcha, $method)) {
             return call_user_func_array(array($this->captcha, $method), $args);
         }
+
+        if (method_exists($this->captcha->get_CaptchaBase(), $method)) {
+            return call_user_func_array(array($this->captcha->get_CaptchaBase(), $method), $args);
+        }
+    }
+
+    /**
+     * @return object
+     */
+    public function getCaptchaInstance() {
+        return $this->captcha;
     }
 
     /**
@@ -118,6 +135,10 @@ class CaptchaComponent extends Component
      */
     public function __get($name)
     {
+        if (method_exists($this->captcha->get_CaptchaBase(), ($method = 'get_'.$name))) {
+            return $this->captcha->get_CaptchaBase()->$method();
+        }
+
         if (method_exists($this->captcha, ($method = 'get_'.$name))) {
             return $this->captcha->$method();
         }
@@ -129,9 +150,13 @@ class CaptchaComponent extends Component
 
     public function __isset($name)
     {
+        if (method_exists($this->captcha->get_CaptchaBase(), ($method = 'isset_'.$name))) {
+            return $this->captcha->get_CaptchaBase()->$method();
+        }
+
         if (method_exists($this->captcha, ($method = 'isset_'.$name))) {
             return $this->captcha->$method();
-        } 
+        }
 
         if (method_exists($this, ($method = 'isset_'.$name))) {
             return $this->$method();
@@ -140,6 +165,10 @@ class CaptchaComponent extends Component
 
     public function __set($name, $value)
     {
+        if (method_exists($this->captcha->get_CaptchaBase(), ($method = 'set_'.$name))) {
+            return $this->captcha->get_CaptchaBase()->$method($value);
+        }
+
         if (method_exists($this->captcha, ($method = 'set_'.$name))) {
             $this->captcha->$method($value);
         } else if (method_exists($this, ($method = 'set_'.$name))) {
@@ -149,6 +178,10 @@ class CaptchaComponent extends Component
 
     public function __unset($name)
     {
+        if (method_exists($this->captcha->get_CaptchaBase(), ($method = 'unset_'.$name))) {
+            return $this->captcha->get_CaptchaBase()->$method();
+        }
+
         if (method_exists($this->captcha, ($method = 'unset_'.$name))) {
             $this->captcha->$method();
         } else if (method_exists($this, ($method = 'unset_'.$name))) {
