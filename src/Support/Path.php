b78911cb493805af20684c7e863c212e8080492e
botdetect.support <botdetect.support@captcha.com>
2019-02-14 18:38:45 +0700

    read BD PHP resources from "botdetect-captcha-lib" dir

--- src/Support/Path.php
+++ src/Support/Path.php
@@ -38,7 +38,18 @@ final class Path
      */
     public static function getCaptchaLibPath()
     {
-        return __DIR__ . '/../../../captcha/';
+        $libPath1 = __DIR__ . '/../../../captcha/botdetect-captcha-lib';
+        $libPath2 = __DIR__ . '/../../../captcha/lib';
+
+        if (is_dir($libPath1)) {
+            return $libPath1;
+        }
+
+        if (is_dir($libPath2)) {
+            return $libPath2;
+        }
+
+        return null;
     }
 
     /**
@@ -48,7 +59,7 @@ final class Path
      */
     public static function getPublicDirPathInLibrary()
     {
-        return self::getCaptchaLibPath() . 'lib/botdetect/public/';
+        return self::getCaptchaLibPath() . '/botdetect/public/';
     }
 
     /**
